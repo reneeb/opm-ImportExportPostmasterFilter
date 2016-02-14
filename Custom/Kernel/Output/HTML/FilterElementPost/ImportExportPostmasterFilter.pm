123a939212f7ad32a0b8aabf7157a46697534cca
reneeb <reb@perl-services.de>
2016-02-14 13:21:07 +0100

    update output filter (package name, copyright and object dependencies)

--- Custom/Kernel/Output/HTML/FilterElementPost/ImportExportPostmasterFilter.pm
+++ Custom/Kernel/Output/HTML/FilterElementPost/ImportExportPostmasterFilter.pm
@@ -1,23 +1,22 @@
 # --
-# Kernel/Output/HTML/OutputFilterImportExportPostmasterFilter.pm
-# Copyright (C) 2014 Perl-Services.de, http://www.perl-services.de/
+# Kernel/Output/HTML/FilterElementPost/ImportExportPostmasterFilter.pm
+# Copyright (C) 2014 - 2016 Perl-Services.de, http://www.perl-services.de/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::Output::HTML::OutputFilterImportExportPostmasterFilter;
+package Kernel::Output::HTML::FilterElementPost::ImportExportPostmasterFilter;
 
 use strict;
 use warnings;
 
 use List::Util qw(first);
 
-our $VERSION = 0.02;
-
 our @ObjectDependencies = qw(
     Kernel::System::Web::Request
+    Kernel::Output::HTML::Layout
 );
 
 sub new {
